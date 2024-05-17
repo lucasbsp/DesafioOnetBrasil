7d30005367013510211db623081d9d2b7cc40d69
lucasbsp <lucasbspereira@hotmail.com>
2024-05-17 12:22:23 -0300

    Add project files.

---
+++ DesafioOnetBrasil/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DesafioOnetBrasil
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
